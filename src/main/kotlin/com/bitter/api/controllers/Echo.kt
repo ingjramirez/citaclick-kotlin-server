b2a5dbfa83239b4b6200beda35a96b0204e3a503
Jaime Ramirez <jimbo4300@hotmail.com>
2018-09-14 16:41:15 +0200

    Add instance starter

---
+++ src/main/kotlin/com/bitter/api/controllers/Echo.kt
@@ -0,0 +1,15 @@
+package com.bitter.api.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/echo")
+class Echo {
+
+    @GetMapping
+    fun echo(): String {
+        return "Echo"
+    }
+}
